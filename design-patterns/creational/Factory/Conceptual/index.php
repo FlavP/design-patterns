57e45aedb62381d05a3d54f429f8f3a0872f5ef2
Flavius <flaviuspopoiu@gmail.com>
2019-05-30 18:09:53 +0300

    Fixed Chain of Responsability Middleware

--- design-patterns/creational/Factory/Conceptual/index.php
+++ design-patterns/creational/Factory/Conceptual/index.php
@@ -1,1 +1,44 @@
 <?php
+abstract class Creator{
+    abstract function factoryMethod();
+    public function someOperation(){
+        $product = $this->factoryMethod();
+        $result = "" . $product->operation();
+        return $result;
+    }
+}
+class ConcreteCreator1 extends Creator{
+    public function factoryMethod()
+    {
+        return new ConcreteProduct1();
+    }
+}
+class ConcreteCreator2 extends Creator{
+    public function factoryMethod()
+    {
+        return new ConcreteProduct2();
+    }
+}
+interface ProductInterface{
+    public function operation();
+}
+class ConcreteProduct1 implements ProductInterface {
+    public function operation()
+    {
+        return "{Result of the ConcreteProduct1}";
+    }
+}
+class ConcreteProduct2 implements ProductInterface {
+    public function operation()
+    {
+        return "{Result of the ConcreteProduct2}";
+    }
+}
+function clientCode(Creator $creator){
+    echo "Client: I'm not aware of the creator's class, but it still works.\n " . $creator->someOperation();
+}
+echo "App: Launched with the ConcreteCreator1.\n";
+clientCode(new ConcreteCreator1);
+echo "\n\n";
+echo "App: Launched with the ConcreteCreator2.\n";
+clientCode(new ConcreteCreator2);
